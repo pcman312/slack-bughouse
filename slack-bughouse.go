5659614f5ca6900ca3c13de9d6cce77c729af621
Jim Turpin <jim@openwager.com>
2017-08-07 15:57:37 -0600

    actually write messages for slack to post

--- slack-bughouse.go
+++ slack-bughouse.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"net/http"
@@ -19,24 +20,13 @@ func test(rw http.ResponseWriter, req *http.Request) {
 
 	req.ParseForm()
 
-	/*/ Parse form values
-	fmt.Println("token:", req.Form["token"])
-	fmt.Println("team_id:", req.Form["team_id"])
-	fmt.Println("team_domain:", req.Form["team_domain"])
-	fmt.Println("user_name:", req.Form["user_name"])
-	fmt.Println("text:", req.Form["text"])
-	*/
-	fmt.Println(req.Form["text"][0])
-	fmt.Println("text:", req.Form["text"])
 	names := strings.Split(req.Form["text"][0], " ")
-	fmt.Printf("%T", names)
-	fmt.Println(names)
 
 	if len(names) < 4 {
 		fmt.Println("There are not enough players!")
 
 	} else {
-		//fmt.Println("Let the games begin!")
+		//  Let the games begin!
 		t := time.Now()
 		rand.Seed(int64(t.Nanosecond())) // no shuffling without this line
 
@@ -44,7 +34,9 @@ func test(rw http.ResponseWriter, req *http.Request) {
 			j := rand.Intn(i)
 			names[i], names[j] = names[j], names[i]
 		}
-		fmt.Println("Team 1 White: ", names[1], "\nTeam 1 Black: ", names[2], "\nTeam 2 Black: ", names[3], "\nTeam 2 Black: ", names[3])
+
+		results := "Team 1 White: " + names[0] + "\nTeam 1 Black: " + names[1] + "\nTeam 2 White: " + names[2] + "\nTeam 2 Black: " + names[3]
+		io.WriteString(rw, results)
 	}
 
 }
