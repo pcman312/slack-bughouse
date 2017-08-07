678d8b43b4c99f39da86ab2c66e1f24b893bf4ef
Jim Turpin <jim@openwager.com>
2017-08-07 14:16:48 -0600

    parse post data

--- slack-bughouse.go
+++ slack-bughouse.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -16,6 +17,25 @@ func test(rw http.ResponseWriter, req *http.Request) {
 		panic(err)
 	}
 	log.Println(string(body))
+
+	if req.Method == "POST" {
+		req.ParseForm()
+		token := req.Form.Get("token")
+		if token != "" {
+			fmt.Println("token is:", token)
+		} else {
+			fmt.Println("I see no token")
+		}
+		// logic part of log in
+		fmt.Println("token:", req.Form["token"])
+		fmt.Println("team_id:", req.Form["team_id"])
+
+		fmt.Println("team_domain:", req.Form["team_domain"])
+		fmt.Println("user_name:", req.Form["user_name"])
+		fmt.Println("text:", req.Form["text"])
+
+	}
+
 }
 
 func main() {
