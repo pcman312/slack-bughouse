e93d5a4fbb1cc3f8bb0d11ecf60d01a50b8d2be6
michael.golowka <michael.golowka@logrhythm.com>
2018-08-21 10:53:17 -0600

    Updated cloudbuild and docker-compose

--- tests/test-it.sh
+++ tests/test-it.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -v http://slack-bughouse:9090/teams?text=player1%20player2%20player3%20player4
+curl --connect-timeout 2 -v http://slack-bughouse:9090/teams?text=player1%20player2%20player3%20player4
+#curl --connect-timeout 2 -v http://slack-bughouse:9091/teams?text=player1%20player2%20player3%20player4
